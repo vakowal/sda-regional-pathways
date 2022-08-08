f6c543916c381c5c9dce80e4242e9d4c5944ad28
vakowal <ginger.kowal@gmail.com>
2022-08-08 13:33:57 -0400

    implement default calculation of SDA intensity pathway

--- scripts/00_packages_functions_globals.R
+++ scripts/00_packages_functions_globals.R
@@ -13,7 +13,48 @@ script_dir <- dirname(getSourceEditorContext()$path)
 repo_dir <- dirname(script_dir)
 
 # Custom functions
-CalcIntensityPathway <- function()
+
+# Calculate intensity pathway.
+# Args:
+#  - company activity (vector): activity (m2) of company by year, from base
+#    year to target year
+#  - sector activity (vector): activity (m2) of sector by year, from base year
+#    to target year
+#  - sector_emissions (vector): emissions (MtCO2) of sector by year, from base
+#    year to target year
+#  - company_emissions_base (scalar): emissions (MtCO2) of company in base year
+# Returns:
+#  - company_intensity target (vector), a vector of intensity (kCO2/m2) from
+#    base year to target year
+# IMPORTANT: target year must be 2050
+CalcIntensityPathway <- function(
+    company_activity, company_emissions_base, 
+    sector_activity, sector_emissions){
+  sector_intensity <- (sector_emissions * 1000000) / sector_activity
+  SI_2050 <- sector_intensity[length(sector_intensity)]  # final item must describe 2050
+  CI_base <- (company_emissions_base * 1000) / company_activity[1]  # TODO check units here
+  d_diff_param <- CI_base - SI_2050
+  m_param <- CalcMParam(company_activity, sector_activity)
+  p_convergence_idx <- (
+    (sector_intensity - SI_2050) / (sector_intensity[1] - SI_2050))
+  company_intensity_target <- (
+    d_diff_param * p_convergence_idx * m_param + SI_2050)
+  return(company_intensity_target)
+}
+
+# Default calculation of m parameter.
+# Args:
+#   - company activity (vector): activity (m2) of company by year
+#   - sector activity (vector): activity (m2) of sector by year
+# The two vectors must have identical length and must end with the target year.
+CalcMParam <- function(company_activity, sector_activity){
+  target_year_ratio <- (
+    company_activity[length(company_activity)] /
+      sector_activity[length(sector_activity)])
+  m_param <- (company_activity / sector_activity) / target_year_ratio
+  m_param[m_param > 1] <- 1
+  return(m_param)
+}
 
 # Project directories
 wd <- list()
--- scripts/02_analysis_and_plots.R
+++ scripts/02_analysis_and_plots.R
@@ -10,8 +10,29 @@ script_dir <- dirname(getSourceEditorContext()$path)
 
 # package imports, function definitions, and globals including directory
 source(paste0(script_dir, "/00_packages_functions_globals.R"))
-## WTF this is not working!???!
 
+# calculate an example intensity pathway
+res_sector_data <- read.csv(
+  paste0(wd$raw_data, 'residential_buildings_sector_data.csv'))
+
+# example input data
+base_year <- 2020
+company_emissions_base <- 7000000
+company_activity_base <- 10000000
+company_activity_target <- 12000000
+# calculate yearly activity
+company_activity <- seq(
+  company_activity_base, company_activity_target,
+  length.out=(2050-base_year) + 1)
+sector_activity <- res_sector_data[
+  res_sector_data$year >= base_year, 'Sector_activity']
+sector_emissions <- res_sector_data[
+  res_sector_data$year >= base_year, 'Sector_Scope_1_emissions']
+
+example_pathway <- CalcIntensityPathway(
+  company_activity, company_emissions_base, sector_activity, sector_emissions)
+
+## DEMONSTRATION, THROWAWAY ##
 # do analysis
 example_processed_data <- read.csv(
   paste0(wd$processed_data, 'example_processed_data.csv'))
