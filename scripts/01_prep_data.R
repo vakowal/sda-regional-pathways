e58b741b3b1ab419e06ee8d59a8ab72cbe72a9c3
Eamonn Fetherston <eamonn.fetherston@gmail.com>
2022-08-10 12:09:21 -0500

    loaded in and cleaned data for CRREM EU residential pathways

--- scripts/01_prep_data.R
+++ scripts/01_prep_data.R
@@ -10,7 +10,30 @@ script_dir <- dirname(getSourceEditorContext()$path)
 # package imports, function definitions, and globals including directory
 source(paste0(script_dir, "/00_packages_functions_globals.R"))
 
+
 # read data, clean and process
+multi_family <- read.csv(paste0(wd$raw_data, "2022-08-10_CRREM-multi-family.csv"))
+single_family <- read.csv(paste0(wd$raw_data, "2022-08-10_CRREM-single-family.csv"))
+
+multi_family <- multi_family %>% 
+  pivot_longer(!ï..YEAR, names_to = "country", values_to = "emissions_intensity")  
+  
+multi_family$building_type <- "Multi-family residential"
+colnames(multi_family)[1] <- "year"
+
+single_family <- single_family %>% 
+  pivot_longer(!ï..YEAR, names_to = "country", values_to = "emissions_intensity")
+
+single_family$building_type <- "Single-family residential"
+colnames(single_family)[1] <- "year"
+
+CRREM_EU_residential <- union(multi_family, single_family)
+
+
+
+
+## DEMONSTRATION, THROWAWAY ##
+# demo: read data, clean and process
 example_data <- read.csv(paste0(wd$raw_data, "sda_pathways.csv"))
 example_data$intensity_base_year_scope1_2 <- (
   (example_data$Base.year.scope.1.emissions..tCO2. + 
--- scripts/02_analysis_and_plots.R
+++ scripts/02_analysis_and_plots.R
@@ -12,15 +12,31 @@ script_dir <- dirname(getSourceEditorContext()$path)
 source(paste0(script_dir, "/00_packages_functions_globals.R"))
 
 # do analysis
+
+
+# make plots
+
+
+
+
+## DEMONSTRATION, THROWAWAY ##
+# demo: do analysis
 example_processed_data <- read.csv(
   paste0(wd$processed_data, 'example_processed_data.csv'))
 
-# make plots
-example_plot <- ggplot(
-  example_processed_data, aes(
-    x=Model...Scenario, y=intensity_target_year_scope1_2))
-example_plot <- example_plot + geom_point() + facet_wrap(~Region)
-pngname <- paste0(wd$figs, "example_plot.png")
-png(filename=pngname, width=6, height=6, units='in', res=300)
-print(example_plot)
-dev.off()
+# demo: make plots
+ example_plot <- ggplot(example_processed_data, aes(
+    x = Model...Scenario, y = intensity_target_year_scope1_2))
+ 
+
+example_plot <- example_plot + geom_point() + facet_wrap(~Region) 
+ggsave(filename = "example_plot.png", 
+       plot = example_plot,
+       path = wd$figs,
+       width = 6,
+       height = 6,
+       units = "in",
+       dpi = 300)
+
+
+
